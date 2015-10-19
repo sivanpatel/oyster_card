68a99be6e8c15d0eb0795a9c1b7d55e85c82d06a
Sivan Patel <sivan.patel@googlemail.com>
2015-10-19 10:22:37 +0100

    Passing test for top up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,9 @@ describe Oystercard do
     expect(subject.balance).to equal(0)
   end
 
+  it 'should be able to top up to increase the balance' do
+    subject.top_up(1.00)
+    expect(subject.balance).to equal (1.00)
+  end
+
 end
